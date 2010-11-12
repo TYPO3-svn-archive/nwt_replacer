81e1f896c1b6f46b36adf37548fe8ac4fa85ffa2
foertel <foertel@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-12 10:34:46 +0000

    [+SVN] Replacer: initial commit.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/nwt_replacer/trunk@40109 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "fm11_sso".
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Generic Content Replacer',
+	'description' => 'Replaces markers or regex-patterns - registered via TS or static call - in content.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Felix Oertel',
+	'author_email' => 'oertel@networkteam.com',
+	'author_company' => 'networkteam',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.2.0-0.0.0',
+			'typo3' => '4.4.0-4.4.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => '',
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ ext_localconf.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] = 
+	'EXT:nwt_replacer/Classes/Replacer.php:&tx_nwtreplacer_replacer->contentPostProc';
+?>
